93a6c82165df8848e4498c16dc35dfa87a00765e
Zaw Waiyan Lwin <zaw.waiyan.lwin96@gmail.com>
2023-02-22 22:59:43 +0630

    feat[model]: Add position model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Database\Seeders\DepartmentSeeder;
+use Database\Seeders\PositionsSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             DepartmentSeeder::class,
+            PositionsSeeder::class,
         ]);
     }
 }
