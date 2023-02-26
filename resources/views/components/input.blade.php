3b2ba21b2e613e477bfe9a9052b240a8eac20c01
Wai Phyo Thu <wopoto@gmail.com>
2023-02-27 00:21:36 +0630

    Update: dashboard sidemenu, toolbar, login, forgot password UI Design

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,11 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' => 'w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600',
+]) !!}>
+
+
+{{-- <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' =>
+        'w-full pl-8 border-b-2 font-display focus:outline-none focus:border-primarycolor transition-all duration-500 capitalize text-lg',
+]) !!}> --}}
