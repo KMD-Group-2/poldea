8e1f9e3d9ed86e726273a56fc5bc06b5bb7cdd31
Hein Htet <htet37871@gmail.com>
2023-03-02 23:31:19 +0630

    Navigation Username Update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,7 +37,7 @@
                 <!-- ============================================================== -->
 
                 <li class="nav-item dropdown u-pro">
-                    <a class="nav-link dropdown-toggle waves-effect waves-dark profile-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ Auth::user()->staff->photo ?? asset('/assets/images/default-user.png') }}" alt="user" class=""> <span class="hidden-md-down">{{ Auth::user()->username ?? '' }} &nbsp;<i class="fa fa-angle-down"></i></span> </a>
+                    <a class="nav-link dropdown-toggle waves-effect waves-dark profile-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ Auth::user()->staff->photo ?? asset('/assets/images/default-user.png') }}" alt="user" class=""> <span class="hidden-md-down">{{ Auth::user()->staff->name ?? '' }} {{ Auth::user()->getRoleNames()->count() == 0 ? '' : '(' . Auth::user()->getRoleNames()->first() . ')' }} &nbsp;<i class="fa fa-angle-down"></i></span> </a>
                     <div class="dropdown-menu dropdown-menu-right animated flipInY">
                         <!-- text-->
                         <a href="javascript:void(0)" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
