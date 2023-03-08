d15edd231d6b42268e887935986a0f2c6362ef33
Zaw Waiyan Lwin <zaw.waiyan.lwin96@gmail.com>
2023-03-08 23:46:19 +0630

    refactor[UI]: Clean up login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,40 +1,43 @@
 <x-guest-layout>
     <section id="wrapper">
         <div class="login-register">
-            <div class="logo">
-                <x-application-logo type="vertical_color" width="300px" height="300px" />
-            </div>
+            <img class="d-block mx-auto h-25" src="/assets/logo.svg" />
             <div class="login-box card">
                 <div class="card-body">
                     <form class="form-horizontal form-material" id="loginform" action="{{ route('login') }}" method="POST">
                         @csrf
                         <!-- Session Status -->
-                        <x-auth-session-status class="mb-4 alert alert-success alert-rounded" :status="session('status')" />
+                        <x-auth-session-status class="alert alert-success" :status="session('status')" />
 
                         <!-- Validation Errors -->
-                        <x-auth-validation-errors class="mb-4 alert alert-danger alert-rounded" :errors="$errors" />
+                        <x-auth-validation-errors class="alert alert-danger" :errors="$errors" />
 
                         <h3 class="text-left m-b-20">Sign In</h3>
                         <div class="form-group ">
                             <div class="col-xs-12">
-                                <input class="form-control" type="text" name="username" required="" placeholder="Username"> </div>
+                                <input class="form-control" type="text" name="username" placeholder="Username" required> </div>
                         </div>
                         <div class="form-group">
                             <div class="col-xs-12">
-                                <input class="form-control" type="password" name="password" required="" placeholder="Password"> </div>
+                                <input class="form-control" type="password" name="password" placeholder="Password" required> </div>
                         </div>
                         <div class="form-group row">
                             <div class="col-md-12">
                                 <div class="d-flex no-block align-items-center">
                                     <div class="ml-auto">
-                                        <a href="{{ route('password.request') }}" class="text-info"><i class="fas fa-lock m-r-5"></i> Forgot password?</a>
+                                        <a
+                                            href="{{ route('password.request') }}"
+                                            class="text-info"
+                                        >
+                                            <i class="fas fa-lock m-r-5"></i> Forgot password?
+                                        </a>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="form-group text-center">
-                            <div class="col-xs-12 p-b-20">
-                                <button class="btn btn-block btn-lg btn-info btn-rounded" type="submit">Sign In</button>
+                        <div class="text-center">
+                            <div class="col-xs-12">
+                                <button class="btn btn-block btn-lg btn-info" type="submit">Sign In</button>
                             </div>
                         </div>
                     </form>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,47 +5,35 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/default-user.png') }}">
+    <meta name="description" content="Polling website for university departments">
+    <meta name="author" content="group2">
+
+    <link rel="icon" href="/assets/favicon.png" type="image/png" />
+
+    <link rel="stylesheet" href="/css/login.css" />
+    <link rel="stylesheet" href="/css/style.min.css" />
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
-
-    <!-- page css -->
-    <link href="{{ asset('css/pages/login-lock.css') }}" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="{{ asset('css/style.min.css') }}" rel="stylesheet">
 </head>
 
-<body class="skin-default card-no-border">
-    <!-- ============================================================== -->
-    <!-- Preloader - style you can find in spinners.css -->
-    <!-- ============================================================== -->
-    <div class="preloader">
-        <div class="loader">
-            <div class="loader__figure"></div>
-            <p class="loader__label">Poldea</p>
-        </div>
-    </div>
+<body>
+
+    @include('common/loading-screen')
 
     <div>
         {{ $slot }}
     </div>
 
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
-    <script src="{{ asset('assets/plugins/jquery/jquery-3.2.1.min.js') }}"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="{{ asset('assets/plugins/popper/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="/js/jquery-3.2.1.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $(function() {
+            $(".preloader").fadeOut();
+        });
+    </script>
 
-    <!--Custom JavaScript -->
-    <script src="{{ asset('js/pages/login-lock.js') }}"></script>
 </body>
 
 </html>
