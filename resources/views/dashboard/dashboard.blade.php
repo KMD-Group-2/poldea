d19915f4d8a5970f02a8b7ecea6167e1c4d4f48b
Zaw Waiyan Lwin <zaw.waiyan.lwin96@gmail.com>
2023-03-09 13:10:28 +0630

    feat[UI]: Add needed scripts by the layout

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,3 +1,7 @@
 <x-app-layout>
     <x-topbar :username="$username" :role="$role"/>
+    <x-sidebar/>
+    @push('scripts')
+        <script src="/js/dashboard.js"></script>
+    @endpush
 </x-app-layout>
