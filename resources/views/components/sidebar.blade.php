5e1a285c7f080f018352e46afbb79a5d73a6235f
Zaw Waiyan Lwin <zaw.waiyan.lwin96@gmail.com>
2023-03-09 12:32:09 +0630

    feat[UI]: Add sidebar component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,35 @@
+<aside class="left-sidebar">
+    <div class="scroll-sidebar">
+        <nav class="sidebar-nav active">
+            <ul id="sidebar-nav" class="in">
+                @hasanyrole('Admin|QA Manager|QA Coordinator')
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-home"></i>
+                            <span class="hide-menu">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-users"></i>
+                            <span class="hide-menu">Users</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-id-badge"></i>
+                            <span class="hide-menu">Staff</span>
+                        </a>
+                    </li>
+                @else
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-lightbulb"></i>
+                            <span class="hide-menu">Ideas</span>
+                        </a>
+                    </li>
+                @endhasanyrole
+            </ul>
+        </nav>
+    </div>
+</aside>
