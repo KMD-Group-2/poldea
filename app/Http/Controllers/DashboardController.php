b2070d52f8a4c954beec6182f3658cb3bbb57531
Zaw Waiyan Lwin <zaw.waiyan.lwin96@gmail.com>
2023-03-09 16:58:50 +0630

    refactor[Controller]: Remove data passing to view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     public function index(): View
     {
-        $user = Auth::user();
-        $data = [
-            'username' => $user->username,
-            'role' => $user->getRoleNames()->first(),
-        ];
-        return view('dashboard.dashboard', $data);
+        return view('dashboard.dashboard');
     }
 }
